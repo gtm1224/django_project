7855ae59d1dfef70091ab894a5164227caf4a847
Tim Guo <gtm1224@berkeley.edu>
2021-12-18 17:56:58 -0800

    add register function

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm, UserProfileForm
 
 def user_login(request):
     if request.method == 'GET':
@@ -19,3 +19,25 @@ def user_login(request):
                 return HttpResponse('Sorry, your username or password is not right.')
         else:
             return HttpResponse('Invalid login')
+
+
+
+def register(request):
+    if request.method == 'GET':
+        user_form = RegistrationForm()
+        userprofile_form = UserProfileForm()
+        return render(request, 'account/register.html',{'form':user_form,'profile':userprofile_form})
+    if request.method == 'POST':
+        user_form = RegistrationForm(request.POST)
+        userprofile_form = UserProfileForm(request.POST)
+        if user_form.is_valid()*userprofile_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            new_profile = userprofile_form.save(commit=False)
+            new_profile.user = new_user
+            new_profile.save()
+            return HttpResponse('Congratulations! You have successfully registered.')
+        else:
+            return HttpResponse('Sorry. Something went wrong, please try again.')
+
