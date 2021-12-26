d127b8626a44d90e9bdc539a5dfe491616538711
Tim Guo <gtm1224@berkeley.edu>
2021-12-25 22:36:04 -0800

    merry christmas

--- account/models.py
+++ account/models.py
@@ -9,4 +9,12 @@ class UserProfile(models.Model):
         return "user {}".format(self.user.username)
 
 
-# Create your models here.
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,unique=True)
+    company = models.CharField(max_length=100,blank=True)
+    profession = models.CharField(max_length=100,blank=True)
+    aboutme = models.TextField(blank=True)
+
+    def __str__(self):
+        return "user:{}".format(self.user.username)
+
