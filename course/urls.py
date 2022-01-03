c865806a77c123d11a449e231371afc7a053750e
Tim Guo <gtm1224@berkeley.edu>
2022-01-03 13:02:55 -0800

    add mixin and show course for login user

--- course/urls.py
+++ course/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
-from .views import CourseHome
+from .views import CourseHome,CourseListView
 
 app_name = 'course'
 urlpatterns = [
     path('', CourseHome.as_view(), name='home'),
+    path('course-list/',CourseListView.as_view(),name='course_list'),
 ]
--- course/views.py
+++ course/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic import TemplateView,ListView
+from .models import Course
 
 class CourseHome(TemplateView):
     template_name = "course/home.html"
+
+class UserMixin:
+    def get_queryset(self):
+        qs = super(UserMixin,self).get_queryset()
+        return qs.filter(user=self.request.user)
+
+class UserCourseMixin(UserMixin):
+    model = Course
+
+
+
+class CourseListView(UserCourseMixin,ListView):
+
+    context_object_name = 'courses'
+    template_name = 'course/course_list.html'
